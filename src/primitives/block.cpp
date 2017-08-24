3dac9b828db5be0a79b1bfc104e18156d27b1393
Tony Grebnev <tony@merit.me>
2017-08-24 15:27:18 +0300

    fix merkle root

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -18,15 +18,20 @@ uint256 CBlockHeader::GetHash() const
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u, ref=%u)\n",
         GetHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
         hashMerkleRoot.ToString(),
         nTime, nBits, nNonce,
-        vtx.size());
+        vtx.size(),
+        m_vRef.size());
+
     for (const auto& tx : vtx) {
         s << "  " << tx->ToString() << "\n";
+    }
+    for (const auto& ref : m_vRef) {
+        s << "  " << ref->ToString() << "\n";
     }
     return s.str();
 }
