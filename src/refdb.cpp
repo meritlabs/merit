afd086e93217bae28eafd87d31f6b8ad9b0aafab
Josh Ferrerra <josh@merit.me>
2017-08-31 15:37:31 -0700

    add referrals to leveldb

---
+++ src/refdb.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2017 The Merit Foundation developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "refdb.h"
+
+ReferralsViewDB::ReferralsViewDB(size_t nCacheSize, bool fMemory, bool fWipe) :
+    db(GetDataDir() / "referrals", nCacheSize, fMemory, fWipe, true) {}
+
+bool ReferralsViewDB::GetReferral(const uint256& code_hash, MutableReferral& referral) const {
+    return db.Read(code_hash, referral);
+}
+
+bool ReferralsViewDB::InsertReferralCode(const Referral& referral) {
+    return db.Write(referral.m_codeHash, referral);
+}
+
+bool ReferralsViewDB::ReferralCodeExists(const uint256& code_hash) const {
+    return db.Exists(code_hash);
+}
