6afec6851db6b60bf4c09d0c5b2b6f857bdc1f3b
Maxim Khailo <max@foxcommerce.com>
2017-09-01 12:33:40 -0700

    Added naive ANV computation and stub code.

---
+++ src/pog/anv.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2017 The Merit Foundation developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef MERIT_POG_ANV_H
+#define MERIT_POG_ANV_H
+
+#include "primitives/referral.h"
+
+namespace pog 
+{
+    uint64_t ComputeANV(uint256 wallet_id);
+
+} // namespace pog
+
+#endif //MERIT_POG_ANV_H
