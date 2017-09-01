abb30890ad4344591d62d9c2d2e5a6b02ab2aa66
Adil Wali <adil@merit.me>
2017-09-01 08:58:07 -0700

    Read through the referral count when receiving headers (which are actually blocks).
    Since we are bypassing normal block deserialization, we need to manually read through the vRef.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2309,6 +2309,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         for (unsigned int n = 0; n < nCount; n++) {
             vRecv >> headers[n];
             ReadCompactSize(vRecv); // ignore tx count; assume it is 0.
+            ReadCompactSize(vRecv); // ignore ref count; assume it is 0.            
         }
 
         if (nCount == 0) {
