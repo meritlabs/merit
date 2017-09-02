0b304bf2f88a4731974b8f3c7cb8424b1ab37a51
Maxim Khailo <max@foxcommerce.com>
2017-09-01 17:46:52 -0700

    First version of the PoG winner selection algorithm.
    This version constructs a discrete distribution using the ANVs of the wallets
    and uses Inverse Transform Sampling to sample from the distribution.
    
    A WalletSelected can return N winners by sampling the distribution based
    on the input hash. The sampling is deterministic and returns the same winners
    given an input hash.
    
    This version samples in memory.

--- src/pog/select.cpp
+++ src/pog/select.cpp
@@ -3,8 +3,110 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "pog/select.h"
+#include "clientversion.h"
+#include <algorithm>
+#include <iterator>
 
 namespace pog 
 {
+    /**
+     * AnvDistribution uses Inverse Transform Sampling. Computing the 
+     * CDF is trivial for for the ANV discrete distribution is easy 
+     * by simply sorting and adding up all the ANVs of the wallets provided.
+     *
+     * Scaling to probabilities is unnecessary because we will use a hash function
+     * to sample into the range between 0-MaxAnv. Since the hash is already
+     * a uniform distribution then it provides a good way to sample into
+     * the distribution of ANVs where those with a bigger ANV are sampled more often. 
+     *
+     * The most expensive part of creating the distribution is sorting the ANVs
+     */
+    AnvDistribution::AnvDistribution(WalletAnvs anvs) : m_inverted(anvs.size())
+    {
+        //It doesn't make sense to sample from an empty distribution.
+            assert(anvs.empty() == false);
+
+        //index anvs by wallet id for convenience. 
+        std::transform(std::begin(anvs), std::end(anvs), std::inserter(m_anvs, std::begin(m_anvs)),
+                [](const WalletAnv& anv) {
+                    return std::make_pair(anv.wallet, anv);
+                });
+
+        assert(m_anvs.size() == anvs.size());
+        
+        //sort from lowest to highest
+        std::sort(std::begin(anvs), std::end(anvs), 
+                [](const WalletAnv& a, const WalletAnv& b) { 
+                    return a.anv < b.anv;
+                });
+
+        assert(m_inverted.size() == anvs.size());
+
+        //back will always return because we assume m_anvs is non-empty
+        m_max_anv = m_inverted.back().anv;
+
+        //compute CDF by adding up all the ANVs 
+        uint64_t previous_anv = 0;
+        std::transform(std::begin(anvs), std::end(anvs), std::begin(m_inverted),
+                [&previous_anv](WalletAnv w) {
+                    w.anv += previous_anv;
+                    previous_anv = w.anv;
+                    return w;
+                });
+    }
+
+    const WalletAnv& AnvDistribution::Sample(const uint256& hash) const
+    {
+        assert(m_inverted.empty() == false);
+
+        //TODO: Should we loop over whole hash?
+        auto selected_anv = hash.GetUint64(0) % m_max_anv;
+
+        //find first inverted Wallet Anv that is greater or equal to the selected value.
+        auto pos = std::lower_bound(std::begin(m_inverted), std::end(m_inverted), 
+                selected_anv,
+                [](const WalletAnv& a, uint64_t selected) {
+                    return a.anv < selected;
+                });
+
+        assert(selected_anv < m_max_anv);
+        assert(pos != std::end(m_inverted)); //it should be impossible to not find an anv
+                                             //because selected_anv must be less than max
+
+        auto selected_wallet = m_anvs.find(pos->wallet);
+        assert(selected_wallet != std::end(m_anvs)); //all anvs in m_inverted must be in 
+                                                    //our index
+
+        return selected_wallet->second;
+    }
+
+    size_t AnvDistribution::Size() const { 
+        return m_inverted.size();
+    }
+
+    WalletSelector::WalletSelector(const WalletAnvs& anvs) : m_distribution{anvs} {}
+
+    /**
+     * Selecting winners from the distribution is deterministic and will return the same
+     * N samples given the same input hash.
+     */
+    WalletAnvs WalletSelector::Select(uint256 hash, size_t n) const
+    {
+        assert(n <= m_distribution.Size());
+        WalletAnvs samples;
+
+        while(n--) {
+            const auto& sampled = m_distribution.Sample(hash);
+
+            //combine hashes and hash to get next sampling value
+            CHashWriter hasher{SER_DISK, CLIENT_VERSION};
+            hasher << hash << sampled.wallet;
+            hash = hasher.GetHash();
+
+            samples.push_back(sampled);
+        }
+
+        return samples;
+    }
 
 } // namespace pog
--- src/pog/select.h
+++ src/pog/select.h
@@ -5,8 +5,44 @@
 #ifndef MERIT_POG_SELECT_H
 #define MERIT_POG_SELECT_H
 
+#include "hash.h"
+#include <vector>
+#include <map>
+
 namespace pog 
 {
+    struct WalletAnv
+    {
+        uint256 wallet;
+        uint64_t anv;
+    };
+
+    using WalletAnvs = std::vector<WalletAnv>;
+    using InvertedAnvs = WalletAnvs;
+    using WalletToAnv = std::map<uint256, WalletAnv>;
+
+    class AnvDistribution
+    {
+        public:
+            AnvDistribution(WalletAnvs anvs);
+            const WalletAnv& Sample(const uint256& hash) const;
+            size_t Size() const;
+
+        private:
+            InvertedAnvs m_inverted;
+            WalletToAnv m_anvs;
+            uint64_t m_max_anv = 0;
+    };
+
+    class WalletSelector
+    {
+        public:
+            WalletSelector(const WalletAnvs& anvs);
+
+            WalletAnvs Select(uint256 hash, size_t n) const;
+        private:
+            AnvDistribution m_distribution;
+    };
 
 } // namespace pog
 
