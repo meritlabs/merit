484ab228a760850177fc2b8096cef3d262c0a119
Maxim Noah Khailo <max@merit.me>
2017-09-06 15:14:09 -0700

    Used GetChildren from ReferralsViewDB

--- src/pog/anv.cpp
+++ src/pog/anv.cpp
@@ -3,50 +3,47 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "pog/anv.h"
-
 #include <stack>
 
 namespace pog 
 {
-    using wallet_stack = std::stack<uint256>;
-    using wallet_ids = std::vector<uint256>;
+    using KeyStack = std::stack<CKeyID>;
 
-    uint64_t GetWalletBalance(const uint256& )
+    uint64_t GetKeyBalance(const CKeyID& key)
     {
         //TODO: make work
         return 0;
     }
 
-    wallet_ids GetReferredIds(const uint256& wallet_id)
+    ChildKeys GetReferredIds(const CKeyID& key, const ReferralsViewDB& db)
     {
-        //TODO: make work
-        return {};
+        return db.GetChildren(key);
     }
 
     /**
      * Aggregate Network Value is computed by walking the referral tree down from the 
-     * wallet_id specified and aggregating each child's ANV.
+     * key_id specified and aggregating each child's ANV.
      *
      * This is the naive version that computes whole tree every time. We need an 
      * accumulated version instead where ANV is cached and gets updated for every
      * mined transaction.
      */
-    uint64_t ComputeANV(uint256 top_wallet_id, ReferralsViewCache& referrals)
+    uint64_t ComputeANV(const CKeyID& top_key_id, const ReferralsViewDB& db)
     {
-        wallet_stack wallets;
-        wallets.push(top_wallet_id);
+        KeyStack keys;
+        keys.push(top_key_id);
 
         uint64_t anv = 0;
-        while(!wallets.empty())
+        while(!keys.empty())
         {
             //aggregate balance
-            const auto& wallet_id = wallets.top();
-            anv += GetWalletBalance(wallet_id);
-            wallets.pop();
+            const auto& key_id = keys.top();
+            anv += GetKeyBalance(key_id);
+            keys.pop();
 
-            auto children = GetReferredIds(wallet_id);
+            auto children = GetReferredIds(key_id, db);
             for(const auto& child: children)
-                wallets.push(child);
+                keys.push(child);
         }
 
         return anv;
