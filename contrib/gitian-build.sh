4cafd835cf7112f6e0a1aceadaeecb8e22d380bb
Adil Wali <adil@merit.me>
2018-02-08 20:20:32 +0300

    Adding merit gitian sig repos.  Also, proper osslsigncode location.

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -22,7 +22,7 @@ proc=2
 mem=2000
 lxc=true
 osslTarUrl=http://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.7.1.tar.gz
-osslPatchUrl=https://meritcore.org/cfields/osslsigncode-Backports-to-1.7.1.patch
+osslPatchUrl=https://bitcoincore.org/cfields/osslsigncode-Backports-to-1.7.1.patch
 scriptName=$(basename -- "$0")
 signProg="gpg --detach-sign"
 commitFiles=true
@@ -232,8 +232,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/bitcoin-core/gitian.sigs.git
-    git clone https://github.com/bitcoin-core/merit-detached-sigs.git
+    git clone https://github.com/meritlabs/gitian.sigs.git
+    git clone https://github.com/meritlabs/merit-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
