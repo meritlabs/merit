a0dc532b4bdaf48a81c8ac23a0628873457aedb6
Josh Ferrerra <josh@merit.me>
2018-02-21 15:28:25 -0800

    move qrimagewidget to its own file

---
+++ src/qt/qrimagewidget.h
@@ -0,0 +1,33 @@
+#ifndef QRIMAGEWIDGET_H
+#define QRIMAGEWIDGET_H
+
+#include <QLabel>
+
+QT_BEGIN_NAMESPACE
+class QMenu;
+QT_END_NAMESPACE
+
+/* Label widget for QR code. This image can be dragged, dropped, copied and saved
+ * to disk.
+ */
+class QRImageWidget : public QLabel
+{
+    Q_OBJECT
+
+public:
+    explicit QRImageWidget(QWidget *parent = 0);
+    QImage exportImage();
+
+public Q_SLOTS:
+    void saveImage();
+    void copyImage();
+
+protected:
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void contextMenuEvent(QContextMenuEvent *event);
+
+private:
+    QMenu *contextMenu;
+};
+
+#endif
--- src/qt/receiverequestdialog.cpp
+++ src/qt/receiverequestdialog.cpp
@@ -12,12 +12,6 @@
 #include "optionsmodel.h"
 #include "walletmodel.h"
 
-#include <QClipboard>
-#include <QDrag>
-#include <QMenu>
-#include <QMimeData>
-#include <QMouseEvent>
-#include <QPixmap>
 #if QT_VERSION < 0x050000
 #include <QUrl>
 #endif
@@ -29,66 +23,6 @@
 #ifdef USE_QRCODE
 #include <qrutil.h>
 #endif
-
-QRImageWidget::QRImageWidget(QWidget *parent):
-    QLabel(parent), contextMenu(0)
-{
-    contextMenu = new QMenu(this);
-    QAction *saveImageAction = new QAction(tr("&Save Image..."), this);
-    connect(saveImageAction, SIGNAL(triggered()), this, SLOT(saveImage()));
-    contextMenu->addAction(saveImageAction);
-    QAction *copyImageAction = new QAction(tr("&Copy Image"), this);
-    connect(copyImageAction, SIGNAL(triggered()), this, SLOT(copyImage()));
-    contextMenu->addAction(copyImageAction);
-}
-
-QImage QRImageWidget::exportImage()
-{
-    if(!pixmap())
-        return QImage();
-    return pixmap()->toImage();
-}
-
-void QRImageWidget::mousePressEvent(QMouseEvent *event)
-{
-    if(event->button() == Qt::LeftButton && pixmap())
-    {
-        event->accept();
-        QMimeData *mimeData = new QMimeData;
-        mimeData->setImageData(exportImage());
-
-        QDrag *drag = new QDrag(this);
-        drag->setMimeData(mimeData);
-        drag->exec();
-    } else {
-        QLabel::mousePressEvent(event);
-    }
-}
-
-void QRImageWidget::saveImage()
-{
-    if(!pixmap())
-        return;
-    QString fn = GUIUtil::getSaveFileName(this, tr("Save QR Code"), QString(), tr("PNG Image (*.png)"), nullptr);
-    if (!fn.isEmpty())
-    {
-        exportImage().save(fn);
-    }
-}
-
-void QRImageWidget::copyImage()
-{
-    if(!pixmap())
-        return;
-    QApplication::clipboard()->setImage(exportImage());
-}
-
-void QRImageWidget::contextMenuEvent(QContextMenuEvent *event)
-{
-    if(!pixmap())
-        return;
-    contextMenu->exec(event->globalPos());
-}
 
 ReceiveRequestDialog::ReceiveRequestDialog(QWidget *parent) :
     QDialog(parent),
