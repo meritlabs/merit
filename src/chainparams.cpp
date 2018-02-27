00bfc7df7977b319ccc9b7e13d55708c8bac6d62
Adil Wali <adil@merit.me>
2018-02-27 11:24:18 -0800

    Adding false for bits filtering.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -187,16 +187,16 @@ public:
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.clear();
-        vSeeds.emplace_back("seed.merit.me");
-        vSeeds.emplace_back("seed.brightkey.io");
-        vSeeds.emplace_back("seed.adil.io");
-        vSeeds.emplace_back("seed.lvlup.com");
-        vSeeds.emplace_back("merit.crypvix.com");
-        vSeeds.emplace_back("seeds.merit.blockcasts.com");
-        vSeeds.emplace_back("merit.commerce6.com");
-        vSeeds.emplace_back("seeds.venturebot.us");
-        vSeeds.emplace_back("seed.firstpri.me");
-        vSeeds.emplace_back("seeds.merit.rocks");
+        vSeeds.emplace_back("seed.merit.me", false);
+        vSeeds.emplace_back("seed.brightkey.io", false);
+        vSeeds.emplace_back("seed.adil.io", false);
+        vSeeds.emplace_back("seed.lvlup.com", false);
+        vSeeds.emplace_back("merit.crypvix.com", false);
+        vSeeds.emplace_back("seeds.merit.blockcasts.com", false);
+        vSeeds.emplace_back("merit.commerce6.com", false);
+        vSeeds.emplace_back("seeds.venturebot.us", false);
+        vSeeds.emplace_back("seed.firstpri.me", false);
+        vSeeds.emplace_back("seeds.merit.rocks", false);
 
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
