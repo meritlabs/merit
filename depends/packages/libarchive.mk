2b59529121a690af23ee7572b29d86e48ca2a8c8
Maxim Khailo <max@merit.me>
2018-03-21 01:53:59 -0700

    Adding libarchive to the depends

---
+++ depends/packages/libarchive.mk
@@ -0,0 +1,25 @@
+package=libarchive
+$(package)_version=3.3.2
+$(package)_download_path=http://www.libarchive.org/downloads
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
+
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared
+  $(package)_config_opts_linux=--with-pic
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+endef
