6611b22cbc096cb7ea437e27549be8abc462ba44
Maxim Khailo <max@merit.me>
2018-03-26 02:59:14 -0700

    Builds go under build directory with cmake

--- depends/packages/libarchive.mk
+++ depends/packages/libarchive.mk
@@ -3,9 +3,14 @@ $(package)_version=3.3.2
 $(package)_download_path=http://www.libarchive.org/downloads
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
+$(package)_build_subdir=build
+
+define $(package)_preprocess_cmds
+  mkdir build
+endef
 
 define $(package)_config_cmds
-	cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix)
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix) ..
 endef
 
 define $(package)_build_cmds
@@ -14,7 +19,4 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
 endef
