a143b2ab559661cee579dd27a3bbc91cf3accc72
Maxim Khailo <max@merit.me>
2018-03-26 01:04:15 -0700

    Fix build of libarchive on windows.

--- depends/packages/libarchive.mk
+++ depends/packages/libarchive.mk
@@ -4,13 +4,8 @@ $(package)_download_path=http://www.libarchive.org/downloads
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
 
-define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
-  $(package)_config_opts_linux=--with-pic
-endef
-
 define $(package)_config_cmds
-  $($(package)_autoconf)
+	camke -DCMAKE_INSTALL_PREFIX=$(build_prefix)
 endef
 
 define $(package)_build_cmds
