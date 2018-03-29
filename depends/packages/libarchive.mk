5cfebc249fa93d3e17248b87b5ee1e56d6d52602
Maxim Khailo <max@merit.me>
2018-03-29 05:54:53 -0700

    Fix prefix to where libarchive is installed.

--- depends/packages/libarchive.mk
+++ depends/packages/libarchive.mk
@@ -3,14 +3,12 @@ $(package)_version=3.3.2
 $(package)_download_path=http://www.libarchive.org/downloads
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
-$(package)_build_subdir=cmbuild
 
 define $(package)_preprocess_cmds
-  mkdir cmbuild
 endef
 
 define $(package)_config_cmds
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix) ..
+	cmake -DCMAKE_INSTALL_PREFIX=$(host_prefix) .
 endef
 
 define $(package)_build_cmds
