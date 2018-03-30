9881e164f8bd6c1a01520983793ed33c0b9be8b4
Maxim Khailo <max@merit.me>
2018-03-29 17:37:06 -0700

    Adding patches to libarchive

--- depends/packages/libarchive.mk
+++ depends/packages/libarchive.mk
@@ -1,14 +1,23 @@
 package=libarchive
 $(package)_version=3.3.2
-$(package)_download_path=http://www.libarchive.org/downloads
+$(package)_download_path=http://www.libarchive.org/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
+$(package)_patches=libarchive-1-fixes.patch
 
 define $(package)_preprocess_cmds
+   patch -p1 < $($(package)_patch_dir)/libarchive-1-fixes.patch
 endef
 
+define $(package)_set_vars
+  $(package)_config_opts=--disable-bsdtar --disable-bsdcpio --disable-bsdcat --disable-xml2 --without-cng --with-nettle
+  $(package)_config_opts_release=--disable-debug-mode
+  $(package)_config_opts_linux=--with-pic
+endef
+
+
 define $(package)_config_cmds
-	cmake -DCMAKE_INSTALL_PREFIX=$(host_prefix) .
+  	$($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
