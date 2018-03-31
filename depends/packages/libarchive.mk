9c81628a1be742e82a8a2029da578c59cd89686a
Maxim Khailo <max@merit.me>
2018-03-31 14:49:14 -0700

    Fix zip support in Windows build.

--- depends/packages/libarchive.mk
+++ depends/packages/libarchive.mk
@@ -4,6 +4,7 @@ $(package)_download_path=http://www.libarchive.org/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
 $(package)_patches=libarchive-1-fixes.patch
+$(package)_dependencies=zlib
 
 define $(package)_preprocess_cmds
    patch -p1 < $($(package)_patch_dir)/libarchive-1-fixes.patch
