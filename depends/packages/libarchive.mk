e5a6985162c02998c3de0e407d89620e7c2861fb
Maxim Khailo <max@merit.me>
2018-04-01 01:27:28 -0700

    Fix libarchive download

--- depends/packages/libarchive.mk
+++ depends/packages/libarchive.mk
@@ -1,6 +1,6 @@
 package=libarchive
 $(package)_version=3.3.2
-$(package)_download_path=http://www.libarchive.org/downloads/
+$(package)_download_path=http://www.libarchive.org/downloads
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
 $(package)_patches=libarchive-1-fixes.patch
