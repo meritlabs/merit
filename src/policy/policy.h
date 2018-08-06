749807355d619db7ed26a44eef4b84d60fdd6825
Maxim Khailo <max@merit.me>
2018-08-06 00:45:44 -0700

    Set default miner thresholds to max block size.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -19,14 +19,14 @@ class CCoinsViewCache;
 class CTxOut;
 
 /** Default for -blockmaxsize, which controls the maximum size of block the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 750000;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 16000000;
 /**
  * Default for -blocktxsmaxsizeshare, which controls the maximum share of transacitons size in block the mining code will create
  * Left space is used for referrals
  */
 static const unsigned int DEFAULT_BLOCK_TRANSACTIONS_MAX_SIZE_SHARE = 90;
 /** Default for -blockmaxweight, which controls the range of block weights the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_WEIGHT = 3000000;
+static const unsigned int DEFAULT_BLOCK_MAX_WEIGHT =16000000;
 /** Default for -blockmintxfee, which sets the minimum feerate for a transaction in blocks created by mining code **/
 static const unsigned int DEFAULT_BLOCK_MIN_TX_FEE = 1000;
 /** The maximum weight for transactions we're willing to relay/mine */
